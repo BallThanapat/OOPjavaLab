343fd0cee462cdbd79ca0834a6661a5ccdfc608b
BallThanapat <115175213+BallThanapat@users.noreply.github.com>
2023-02-24 12:18:37 +0700

    Update Fraction.java

--- week4/Fraction.java
+++ week4/Fraction.java
@@ -1,13 +1,21 @@
 public class Fraction {
-    int topN, btmN;
+    public int topN;
+    public int btmN;
     public String toFraction(){
         return (topN+"/"+btmN);
     }
-    public float toFloat(){
-        return (topN/btmN);
+    public String toFloat(){
+        double sum = (double)topN/(double)btmN;
+        return ""+sum;
     }
     public void addFraction(Fraction f){
-        
+        if (btmN == f.btmN){
+            topN = topN+f.topN;
+            btmN = f.btmN;
+        }else if (btmN != f.btmN){
+            topN = (topN*f.btmN)+(f.topN*btmN);
+            btmN = btmN*f.btmN;
+        }
+        return;
     }
-    
 }
