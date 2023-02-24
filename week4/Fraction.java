cab923063954ef82f23b0e34cb1125c291427ae3
BallThanapat <115175213+BallThanapat@users.noreply.github.com>
2023-02-24 10:19:59 +0700

    Update Fraction.java

--- week4/Fraction.java
+++ week4/Fraction.java
@@ -1,3 +1,13 @@
 public class Fraction {
+    int topN, btmN;
+    public String toFraction(){
+        return (topN+"/"+btmN);
+    }
+    public float toFloat(){
+        return (topN/btmN);
+    }
+    public void addFraction(Fraction f){
+        
+    }
     
 }
