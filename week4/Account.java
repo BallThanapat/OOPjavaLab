e417e9a94c2bb062ac46d37afd2c6daac373b558
BallThanapat <115175213+BallThanapat@users.noreply.github.com>
2023-02-24 14:21:40 +0700

    Add files via upload

---
+++ week4/Account.java
@@ -0,0 +1,27 @@
+public class Account {
+    public double balance;
+    public String name;
+    public void deposit(double b){
+        if(b>=0){
+            balance += b;
+        }else{
+            System.out.println("The balance variable must be greater than or equal to zero.");
+        }
+    }
+    public double withdraw(double b){
+        if((balance-b)<0){
+            System.out.println("Your account balance is insufficient.");
+            b=0;
+        }
+        if(b>=0){
+            balance -= b;
+        }else{
+            System.out.println("The balance variable must be greater than or equal to zero.");
+            b=0;
+        }
+        return b;
+    }
+    public void showInfo(){
+        System.out.println("In "+name+" account, there is a balance equal to "+balance+" baht");
+    }
+}
