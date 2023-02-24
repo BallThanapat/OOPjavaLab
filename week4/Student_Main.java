ad7c464042c4e817db8fbd4bec36478a358c7e0a
BallThanapat <115175213+BallThanapat@users.noreply.github.com>
2023-02-24 10:11:30 +0700

    Update Student_Main.java

--- week4/Student_Main.java
+++ week4/Student_Main.java
@@ -1,11 +1,8 @@
-
-public class Student_Main {
-    public class Main {
+public class mainStudent {
     public static void main(String[] args) {
         Student s = new Student();
         s.mScore = 80;
         s.fScore = 80;
         s.showGrade();
- }
-}
+    }
 }
