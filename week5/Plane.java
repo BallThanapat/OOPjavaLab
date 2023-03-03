80f2701e8f2324483eb76e9c81bad219a10121e9
BallThanapat <115175213+BallThanapat@users.noreply.github.com>
2023-03-03 10:17:10 +0700

    Update Plane.java

--- week5/Plane.java
+++ week5/Plane.java
@@ -1,14 +1,20 @@
 public class Plane {
-    Vehicle p1 = new Vehicle();
+    Vehicle o1 = new Vehicle();
+    Plane b1 = new Plane();
     public void showPlaneInfo(){
-        System.out.println("Plane detail is, "+p1.showInfo());
+        System.out.println("Plane detail is, ");
+        o1.showInfo();
     }
     public void setPlaneInfo(int s, String t){
-        p1.setFuel(s);
-        p1.setTopSpeed(t);
+        o1.setFuel(s);
+        o1.setTopSpeed(t);
     }
     public void fly(){
-        System.out.println("Fly.");
-        
+        if(o1.getFuel()-200>=0){
+            System.out.println("Move.");
+            
+        }else{
+            System.out.println("Please add fuel.");
+        }
     }
 }
