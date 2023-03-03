7a5f802590046a583656bc58861c63ed87956751
BallThanapat <115175213+BallThanapat@users.noreply.github.com>
2023-03-03 10:17:54 +0700

    Update MainVCP.java

--- week5/MainVCP.java
+++ week5/MainVCP.java
@@ -1,8 +1,8 @@
 public class MainVCP {
     public static void main(String[] args) {
-        //Plane p1 = new Plane();
-        //p1.setPlaneInfo(500, "High");
-        //p1.showPlaneInfo();
+        Plane p1 = new Plane();
+        p1.setPlaneInfo(500, "High");
+        p1.showPlaneInfo();
         Car c1 = new Car();
         c1.setCarInfo(500, "High", "Diesel");
         c1.showCarInfo();
