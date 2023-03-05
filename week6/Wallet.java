fc531cdab19b5ad3960a8cc2c55d86a06f15b716
BallThanapat <115175213+BallThanapat@users.noreply.github.com>
2023-03-06 02:47:18 +0700

    Update Wallet.java

--- week6/Wallet.java
+++ week6/Wallet.java
@@ -1,7 +1,7 @@
 public class Wallet {
     private double balance;
     public void setBalance(double balance){
-        balance = this.balance;
+        this.balance = balance;
     }
     public double getBalance(){
         return balance;
