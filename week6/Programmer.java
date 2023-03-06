ab9eb2e239cd36e4a6e873c99c4f214387b68e27
BallThanapat <115175213+BallThanapat@users.noreply.github.com>
2023-03-06 15:37:23 +0700

    Update Programmer.java

--- week6/Programmer.java
+++ week6/Programmer.java
@@ -4,22 +4,28 @@ public class Programmer extends Employee {
         if(getEnergy()>=30){
             System.out.println("Your code is "+str);
             setEnergy(getEnergy()-30);
-            happiness -= 30;
+            setHappiness(getHappiness()-30);
         }else{
             System.out.println("Error Error Error");
             setEnergy(getEnergy()-30);
-            happiness -= 30;
+            setHappiness(getHappiness()-30);
         }
     }
     public void coding(char str){
         if(getEnergy()>=30){
-            System.out.println("Your code is "+str);
-            setEnergy(getEnergy()-30);
-            happiness -= 30;
+            if(this  instanceof SeniorProgrammer){
+                System.out.println("I'm coding about "+str);
+                setEnergy(getEnergy()-5);
+                setHappiness(getHappiness()-5);
+            }else{
+                System.out.println("Your code is "+str);
+                setEnergy(getEnergy()-30);
+                setHappiness(getHappiness()-30);
+            }
         }else{
             System.out.println("Error Error Error");
             setEnergy(getEnergy()-30);
-            happiness -= 30;
+            setHappiness(getHappiness()-30);
         }
     }
     public int getHappiness(){
