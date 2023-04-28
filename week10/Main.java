70ac0288dd20d6fc527ec669bc06043bb9bc1be2
BallThanapat <115175213+BallThanapat@users.noreply.github.com>
2023-04-28 14:54:35 +0700

    Add files via upload

---
+++ week10/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LAB203_79
+ */
+public class Main {
+    public static void main(String[] args) {
+        new App01();
+        new CalculatorSample();
+        new TellerGUI2();
+    }
+}
---
+++ week10/TellerGUI2.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LAB203_79
+ */
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+public class TellerGUI2 implements ActionListener {
+    private JFrame fr;
+    private JPanel p, a, a2;
+    private JButton btn1, btn2, btn3;
+    private JLabel lb1, lb2;
+    private JTextField tf, tf1;
+    private Account ac;
+    public TellerGUI2(){
+        ac = new Account(6000, "");
+        fr = new JFrame("Teller GUI");
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        btn1 = new JButton("Deposit");
+        btn2 = new JButton("Withdraw");
+        btn3 = new JButton("Exit");
+        lb1 = new JLabel("Balance");
+        lb2 = new JLabel("Amount");
+        p = new JPanel();
+        a = new JPanel();
+        a2 = new JPanel();
+        tf = new JTextField((int) ac.getBalance()+"");
+        tf1 = new JTextField();
+        tf.setEditable(false);
+        a.setLayout(new GridLayout(1, 2));
+        a.add(lb1); a.add(tf);
+        a2.setLayout(new GridLayout(1, 2));
+        a2.add(lb2); a2.add(tf1);
+        fr.setLayout(new GridLayout(4, 1));
+
+        fr.add(a, BorderLayout.NORTH);
+        fr.add(a2, BorderLayout.SOUTH);
+
+        p.add(btn1);
+        p.add(btn2);
+        p.add(btn3);
+
+        fr.add(p, BorderLayout.CENTER);
+        btn1.addActionListener(this);
+        btn2.addActionListener(this);
+        btn3.addActionListener(this);
+        fr.pack();
+        fr.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource().equals(btn2)){
+            int amout = Integer.parseInt(tf1.getText());
+            ac.withdraw(amout);
+            tf.setText(ac.getBalance()+"");
+        }
+        else if(e.getSource().equals(btn1)){
+            int amount = Integer.parseInt(tf1.getText());
+            ac.deposit(amount);
+            tf.setText(ac.getBalance()+"");
+        }
+        else if(e.getSource().equals(btn3)){
+            System.exit(0);
+        }
+    }
+}
